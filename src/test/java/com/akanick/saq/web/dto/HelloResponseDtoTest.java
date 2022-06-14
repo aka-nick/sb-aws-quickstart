d38e307d7f27d380adf308b6236b80527f356011
nick <bechalrang@gmail.com>
2022-06-15 00:50:49 +0900

    chapter 1,2 진행 완료

---
+++ src/test/java/com/akanick/saq/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,26 @@
+package com.akanick.saq.web.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+    @Test
+    void 롬복_기능_테스트() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
